525c516ac2adb2e7d196b15446683139d5998004
munyite001 <emunyite@gmail.com>
2023-04-25 09:05:34 +0300

    todo

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,16 +3,18 @@
     Get's the data about an employee
     returns their todo list progress
 """
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
-id = sys.argv[1]
-user = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}').json()
-name = user["name"]
-tasks = requests.get("https://jsonplaceholder.typicode.com/todos").json()
-user_tasks = [task for task in tasks if int(task["userId"]) == int(id)]
-completeTasks = [task for task in user_tasks if task["completed"]]
-tasks = f"{len(completeTasks)}/{len(user_tasks)}"
-print(f"Employee {name} is done with tasks({tasks}):")
-for task in completeTasks:
-    print(f"\t {task['title']}")
+    id = sys.argv[1]
+    user = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
+    user = user.json()
+    name = user["name"]
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    user_tasks = [task for task in tasks if int(task["userId"]) == int(id)]
+    completeTasks = [task for task in user_tasks if task["completed"]]
+    tasks = f"{len(completeTasks)}/{len(user_tasks)}"
+    print(f"Employee {name} is done with tasks({tasks}):")
+    for task in completeTasks:
+        print(f"\t {task['title']}")
