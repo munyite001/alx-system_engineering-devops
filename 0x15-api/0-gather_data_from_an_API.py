d205b5a78dcc596c7b513cd732faac490c027006
munyite001 <emunyite@gmail.com>
2023-04-25 09:03:33 +0300

    todos

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,13 +3,16 @@
     Get's the data about an employee
     returns their todo list progress
 """
-import requests, sys
+import requests
+import sys
 
-user = requests.get(f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}').json()
+id = sys.argv[1]
+user = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}').json()
 name = user["name"]
 tasks = requests.get("https://jsonplaceholder.typicode.com/todos").json()
-user_tasks = [task for task in tasks if int(task["userId"]) == int(sys.argv[1])]
+user_tasks = [task for task in tasks if int(task["userId"]) == int(id)]
 completeTasks = [task for task in user_tasks if task["completed"]]
-print(f"Employee {name} is done with tasks({len(completeTasks)}/{len(user_tasks)}):")
+tasks = f"{len(completeTasks)}/{len(user_tasks)}"
+print(f"Employee {name} is done with tasks({tasks}):")
 for task in completeTasks:
     print(f"\t {task['title']}")
