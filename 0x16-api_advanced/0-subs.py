420dac19c1dee812cd9eb8f471c8618afa686ad2
munyite001 <emunyite@gmail.com>
2023-05-09 21:30:57 +0300

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-number_of_subscribers(subreddit) query the reddit api,
-returns the number of subscribers
-or 0 if subreddit is invalid
+Contains the number_of_subscribers function
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get(f'http://www.reddit.com/r/{subreddit}/about.json',
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subscribers = r.get("data", {}).get("subscribers", 0)
-    return subscribers
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
