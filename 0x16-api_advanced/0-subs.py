e5eab5ad38c22bc2e89df1946cf83a740d6bf84e
munyite001 <emunyite@gmail.com>
2023-05-09 21:25:51 +0300

    subscriber count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,16 +9,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
-
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            return 0
-    except ValueError:
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get(f'http://www.reddit.com/r/{subreddit}/about.json',
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subscribers = r.get("data", {}).get("subscribers", 0)
+    return subscribers
